1184fd99f64d4103196d3d23249b0505b087a9d2
Priyansh <thxlxoo@gmail.com>
2025-08-01 13:19:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,42 +16,87 @@ class Paper {
     currentpaperY = 0;
 
     init(paperElement) {
+        // Mouse events for desktop
         paperElement.addEventListener('mousedown', (e) => {
-            this.holdingpaper = true;
-
-            paperElement.style.zIndex = highestZ;
-            highestZ += 1;
-
-            if (e.button === 0) {
-                this.prevMouseX = this.mouseX;
-                this.prevMouseY = this.mouseY;
-
-                console.log(this.prevMouseX);
-                console.log(this.prevMouseY);
-            }
+            this.startDrag(e.clientX, e.clientY, paperElement);
+            e.preventDefault(); // Prevent text selection
         });
 
         document.addEventListener('mousemove', (e) => {
-            this.mouseX = e.clientX;
-            this.mouseY = e.clientY;
+            this.drag(e.clientX, e.clientY, paperElement);
+        });
 
-            this.velocityX = this.mouseX - this.prevMouseX;
-            this.velocityY = this.mouseY - this.prevMouseY;
+        document.addEventListener('mouseup', () => {
+            this.stopDrag();
+        });
 
+        // Touch events for mobile
+        paperElement.addEventListener('touchstart', (e) => {
+            const touch = e.touches[0];
+            this.startDrag(touch.clientX, touch.clientY, paperElement);
+            e.preventDefault(); // Prevent scrolling and other touch behaviors
+        }, { passive: false });
+
+        document.addEventListener('touchmove', (e) => {
             if (this.holdingpaper) {
-                this.currentpaperX += this.velocityX;
-                this.currentpaperY += this.velocityY;
-
-                this.prevMouseX = this.mouseX;
-                this.prevMouseY = this.mouseY;
-
-                paperElement.style.transform = `translateX(${this.currentpaperX}px) translateY(${this.currentpaperY}px)`;
+                const touch = e.touches[0];
+                this.drag(touch.clientX, touch.clientY, paperElement);
+                e.preventDefault(); // Prevent scrolling while dragging
             }
+        }, { passive: false });
+
+        document.addEventListener('touchend', () => {
+            this.stopDrag();
         });
 
-        window.addEventListener('mouseup', () => {
-            this.holdingpaper = false;
+        // Handle touch cancel (when touch is interrupted)
+        document.addEventListener('touchcancel', () => {
+            this.stopDrag();
         });
+    }
+
+    startDrag(clientX, clientY, paperElement) {
+        this.holdingpaper = true;
+
+        paperElement.style.zIndex = highestZ;
+        highestZ += 1;
+
+        this.prevMouseX = clientX;
+        this.prevMouseY = clientY;
+        this.mouseX = clientX;
+        this.mouseY = clientY;
+
+        // Add a slight scale effect to show the paper is being dragged
+        paperElement.style.transform = `translateX(${this.currentpaperX}px) translateY(${this.currentpaperY}px) scale(1.05)`;
+    }
+
+    drag(clientX, clientY, paperElement) {
+        this.mouseX = clientX;
+        this.mouseY = clientY;
+
+        this.velocityX = this.mouseX - this.prevMouseX;
+        this.velocityY = this.mouseY - this.prevMouseY;
+
+        if (this.holdingpaper) {
+            this.currentpaperX += this.velocityX;
+            this.currentpaperY += this.velocityY;
+
+            this.prevMouseX = this.mouseX;
+            this.prevMouseY = this.mouseY;
+
+            paperElement.style.transform = `translateX(${this.currentpaperX}px) translateY(${this.currentpaperY}px) scale(1.05)`;
+        }
+    }
+
+    stopDrag() {
+        if (this.holdingpaper) {
+            this.holdingpaper = false;
+            // Remove the scale effect when dropping
+            const paperElement = document.elementFromPoint(this.mouseX, this.mouseY)?.closest('.paper');
+            if (paperElement) {
+                paperElement.style.transform = `translateX(${this.currentpaperX}px) translateY(${this.currentpaperY}px) scale(1)`;
+            }
+        }
     }
 }
 
@@ -61,4 +106,3 @@ paperElements.forEach(paperElement => {
     const p = new Paper();
     p.init(paperElement);
 });
-
